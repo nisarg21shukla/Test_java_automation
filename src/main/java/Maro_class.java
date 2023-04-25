9678880fc140274128c646adebfcec0bb7d7e3ce
nisarg21shukla <nisargshukla2121@gmail.com>
2023-04-25 12:43:33 +0530

    chothu commit

--- src/main/java/Maro_class.java
+++ src/main/java/Maro_class.java
@@ -4,6 +4,8 @@ public class Maro_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("yeh duniya");
+        System.out.println("yeh duniya thi");
+        
 	}
 
 }
